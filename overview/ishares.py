5e29381c5c7930f7a112b1b21f6737dffa6514d0
arrows-of-time <arrows-of-time@proton.me>
2023-02-19 17:46:10 +0100

    Add searching for ishares etf's by xetra symbol

--- overview/ishares.py
+++ overview/ishares.py
@@ -1,13 +1,44 @@
+from pymongo.database import Database
+
+from overview.overview import *
 from overview.overview_scrapper import OverviewScrapper
 from etf import Etf
+from bs4 import BeautifulSoup
+
+from utility import get_soup
 
 
-url = ''
+base_url = 'https://www.ishares.com'
+product_list_url = '/de/privatanleger/de/produkte/etf-investments?switchLocale=y&siteEntryPassthrough=true&showAll=true#/?productView=all&pageNumber=1&sortColumn=totalFundSizeInMillions&sortDirection=desc&dataView=keyFacts&keyFacts=all&showAll=true'
 
 
 class IShares(OverviewScrapper):
-    def __init__(self, etf: Etf):
-        super().__init__(etf)
+    def __init__(self, etf: Etf, db: Database):
+        super().__init__(etf, db)
 
     def get_overview(self):
         print(self.etf.isin)
+        product_url = self.search_etf()
+        if product_url is not None:
+            soup = get_soup(base_url + product_url)
+            performance = self.get_performance(soup)
+            portfolio = self.get_portfolio(soup)
+            overview = Overview(self.etf.id, performance, portfolio)
+        else:
+            print(f'No product found for {self.etf.isin}: {self.etf.name}')
+
+    def search_etf(self) -> str | None:
+        soup = get_soup(base_url + product_list_url)
+        a = soup.find('a', string=self.etf.xetra_symbol)
+        if a is not None:
+            return a['href']
+        return None
+
+    def get_performance(self, soup: BeautifulSoup) -> list[Performance]:
+        pass
+
+    def get_portfolio(self, soup: BeautifulSoup) -> Portfolio:
+        pass
+
+    def update_overview(self, overview: Overview):
+        pass
